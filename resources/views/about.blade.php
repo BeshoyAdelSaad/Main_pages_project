7362cc7cdca8bc7ec0bb5bbf7385fb433bb92a19
Besho Adel <beshoadelsaad@gmail.com>
2023-04-24 02:11:59 +0200

    add test syntax

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,80 @@
-  @extends('components.nav')
 
-  @section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+      <link href="{{asset('assets/css/bootstrap-theme.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/css/bootstrap-theme.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/css/swiper.min.css')}}">
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Open+Sans:300,400,700" rel="stylesheet">
+
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>Education Meeting HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+</head>
+<body>
+      <header class="hero">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-6 col-xs-12">
+              <nav>
+                <div id="menu-toggle">
+                  <div class="hamburger">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                  </div>
+                  <div class="cross">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                  </div>
+                </div>
+                <ul class="main-nav">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Portfolio</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+              <!-- <a href="#" class="menu"><img src="assets/menu.png"></a> -->
+              <div class="hero-text">
+                <h1><span>Hi, I am</span><br>Maria Ferrero.</h1>
+                <h3>Freelance designer from Melbourne</h3>
+                <a href="#" class="btn btn-lg btn-primary">Know More</a>
+                <ul class="social-links">
+                  <li class="label">Join me here</li>
+                  <li><a href="#"><img src="{{asset('assets/behance.png')}}"></a></li>
+                  <li><a href="#"><img src="{{asset('assets/dribbble.png')}}"></a></li>
+                  <li><a href="#"><img src="{{asset('assets/twitter.png')}}"></a></li>
+                  <li><a href="#"><img src="{{asset('assets/github.png')}}"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
   <section class="our-facts">
     <div class="container">
       <div class="row">
@@ -51,5 +125,98 @@
       </div>
     </div>
   </section>
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+      <link href="{{asset('assets/css/bootstrap-theme.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/css/bootstrap-theme.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/css/swiper.min.css')}}">
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Open+Sans:300,400,700" rel="stylesheet">
 
-  @endsection
+
+
+      <script src="{{asset('assets/js/jquery-2.1.1.js')}}"></script>
+      <script src="{{asset('assets/js/swiper.jquery.min.js')}}"></script>
+  <script>
+      var swiper1 = new Swiper('.client-swiper', {
+        slidesPerView: 3,
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 60,
+        // Responsive breakpoints
+        breakpoints: {
+          // when window width is <= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            pagination: '.swiper-pagination'
+          },
+          // when window width is <= 480px
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          // when window width is <= 640px
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 30
+          }
+        }
+      });
+      // Initialize Testimonial Swiper
+      var swiper2 = new Swiper('.testimonial-swiper', {
+        slidesPerView: 3,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        freeMode: true,
+        breakpoints: {
+          // when window width is <= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          // when window width is <= 480px
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          // when window width is <= 640px
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
+        }
+      });
+      </script>
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+      <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+      <script>
+      // Counterup
+      $('.counter').counterUp({
+        time: 1000
+      });
+
+      // Main Navigation
+      $('#menu-toggle').click(function(){
+        $(this).toggleClass('open'),
+        $('.main-nav').toggleClass('show-it');
+      })
+      </script>
+
+      <!-- Google Analytics - You should remove this -->
+      <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-29231762-2', 'auto');
+      ga('send', 'pageview');
+
+  </script>
+
+
+</body>
+</html>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,6 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+      <link href="{{asset('assets/css/bootstrap-theme.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/css/bootstrap-theme.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/css/swiper.min.css')}}">
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Open+Sans:300,400,700" rel="stylesheet">
+
+
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">	
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
